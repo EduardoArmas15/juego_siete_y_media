6c3b61dc93f6e417e25c9d531ddbbd619237dd0a
Eduardo Armas Garcia <eduardoarmasgarcia@gmail.com>
2022-08-31 19:52:34 -0500

    first commit

---
+++ JuegoSieteYMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JuegoSieteYMedia
+//
+//  Created by Eduardo Armas Garcia on 31/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+    }
+
+
+}
+
